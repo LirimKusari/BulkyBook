d5efc2c895f44a4d021eb5911f642adba97f3a20
Lirim <kusarilirim@gmail.com>
2022-06-27 13:51:02 +0200

    dropdown and viewbag

---
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,122 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> coverTypeList = _unitOfWork.CoverType.GetAll();
+            return View(coverTypeList);
+        }
+
+
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new();
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }
+            );
+            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }
+            );
+
+            if (id == null || id == 0)
+            {
+                // create product
+                //return NotFound();
+
+                ViewBag.CategoryList = CategoryList;
+                ViewData["CoverTypeList"] = CoverTypeList;
+                return View(product);
+
+            }
+            else
+            {
+                // update product
+
+            }
+
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(CoverType obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Update(obj);
+                _unitOfWork.Save();
+                return RedirectToAction("Index"); // same controller
+                                                  // return RedirectToAction("Index", "ControllerName"); // different controller
+            }
+            TempData["success"] = "CoverType updated successfully";
+            return View(obj);
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            //categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            //var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDbFirst);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.CoverType.Remove(obj);
+            _unitOfWork.Save();
+
+            TempData["success"] = "CoverType deleted successfully";
+            return RedirectToAction("Index"); // same controller
+                                              // return RedirectToAction("Index", "ControllerName"); // different controller
+        }
+
+
+
+
+
+
+
+    }
+}
